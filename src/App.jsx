2a8e17e70aef4befcb038b1d9fd3b6f47ece267d
bayjidalom <bayjidalom1005@gmail.com>
2026-03-18 05:47:12 +0600

    Project initialization

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+
+import './App.css'
+
+
+function App() {
+
+
+  return (
+    <>
+    <nav className='nav-container'>
+      <div className='nav-left'>
+        <img src="" alt="" />
+        <p>Meal Hub</p>
+      </div>
+
+      <div className='nav-right'>
+        <p>Home</p>
+        <p>About</p>
+        <p>Contact</p>
+        <p>Orders</p>
+        <p>Home</p>
+      </div>
+
+    </nav>
+
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = ({num}) => {
+    
+    return (
+        <div>
+            
+         
+        </div>
+    );
+};
+
+export default Home;
